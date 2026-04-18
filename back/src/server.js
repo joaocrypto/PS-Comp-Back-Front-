adbf8cb15648bb51fe4ec342439991ffdfd952aa
João Victor Arantes Oliveira <ojoao830@gmail.com>
2026-04-18 11:10:32 -0300

    :wrench: (back) chore: adiciona e configura .env

--- back/src/server.js
+++ back/src/server.js
@@ -1,11 +1,15 @@
-const routes = require('./routes');
+require('dotenv').config();
 const express = require('express');
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('server is on in port 3000');
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`server is on in port ${PORT}`);
 });
 
