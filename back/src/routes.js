0b5fb70779dbcc50ea60dffcea8270b70667b1fc
João Victor Arantes Oliveira <ojoao830@gmail.com>
2026-04-25 23:57:48 -0300

    feat: Cria schemaValidator do usuario

--- back/src/routes.js
+++ back/src/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const schemaValidator = require('./apps/middlewares/schemaValidator');
 const UsuarioController = require('./apps/controllers/UsuarioController');
+const usuarioSchema = require('./schemas/create_usuario_schema.json');
 
 const routes = new Router();
 
-routes.post('/usuario', UsuarioController.create);
+routes.post('/usuario', schemaValidator(usuarioSchema), UsuarioController.create);
 
 routes.get('/', (req, res) => {
     return res.send({message: 'Connected'});
