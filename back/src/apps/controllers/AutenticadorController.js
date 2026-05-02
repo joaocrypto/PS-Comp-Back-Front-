c51b463f4024d8cc49adfe2c0e1fbd41bc22d186
João Victor Arantes Oliveira <ojoao830@gmail.com>
2026-05-02 17:05:41 -0300

    fix: corrige logica de erro com try catch

--- back/src/apps/controllers/AutenticadorController.js
+++ back/src/apps/controllers/AutenticadorController.js
@@ -7,94 +7,113 @@ const  {encrypt} = require('../../utils/crypt');
 
 class AutenticadorController {
     async autenticar(req, res) {
-        
-        const { email, password } = req.body;
+        try {
+            const { email, password } = req.body;
 
-        const usuario = await Usuarios.findOne({
-            where: { email },
-        });
+            const usuario = await Usuarios.findOne({
+                where: { email },
+            });
 
-        if (!usuario) {
-            return res.status(401).json({ error: 'Usuario não encontrado!'});
+            if (!usuario) {
+                return res.status(401).json({ error: 'Usuario não encontrado!'});
+            }
+
+            if (!await usuario.checkPassword(password)) {
+                return res.status(401).json({ error: 'Senhas não batem!'});
+            }
+
+            const { iv, content } = encrypt(usuario.id);
+            const newId = `${iv}:${content}`;
+
+            const token = jwt.sign({ userId: newId, isAdmin: usuario.is_admin }, process.env.HASH_BCRYPT, {
+                expiresIn: '7d'
+            });
+
+            return res.status(200).json({
+                user: {
+                    id: usuario.id,
+                    user: usuario.user,
+                    is_admin: usuario.is_admin,
+                },
+                token,
+            });
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({ error: 'Erro interno do servidor!'});
         }
-
-        if (!await usuario.checkPassword(password)) {
-            return res.status(401).json({ error: 'Senhas não batem!'});
-        }
-
-        const { iv, content } = encrypt(usuario.id);
-        const newId = `${iv}:${content}`;
-
-        const token = jwt.sign({ userId: newId, is_admin: usuario.is_admin }, process.env.HASH_BCRYPT, {
-            expiresIn: '7d'
-        });
-
-        return res.status(200).json({
-            user: {
-                id: usuario.id,
-                user: usuario.user,
-                is_admin: usuario.is_admin,
-            },
-            token,
-        });
     }
 
 
     async esqueciSenha(req, res) {
-        const { email } = req.body;
-
-        const usuario = await Usuarios.findOne({ where: { email } });
-
-        if (!usuario) {
-            return res.status(404).json({ error: 'Usuário não encontrado' });
-        }
-
-        const codigo = crypto.randomInt(100000, 999999).toString();
-        const expira = new Date();
-        expira.setMinutes(expira.getMinutes() + 15);
-
-        await usuario.update({
-            password_reset_codigo: codigo,
-            password_reset_expira: expira
-        });
-
-
         try {
-            await transport.sendMail({
-                from: 'Suporte Cinerate <noreply@cinerate.com>',
-                to: email,
-                subject: 'Seu código de recuperação de senha',
-                text: `Seu código de recuperação é: ${codigo}`,
-                html: `<b>Seu código de recuperação é: ${codigo}</b><p>Ele expira em 15 minutos.</p>`
+            const { email } = req.body;
+
+            const usuario = await Usuarios.findOne({ where: { email } });
+
+            if (!usuario) {
+                return res.status(404).json({ error: 'Usuário não encontrado' });
+            }
+
+            const codigo = crypto.randomInt(100000, 999999).toString();
+            const expira = new Date();
+            expira.setMinutes(expira.getMinutes() + 15);
+
+            await usuario.update({
+                password_reset_codigo: codigo,
+                password_reset_expira: expira
             });
 
-            return res.json({ message: 'Email enviado com sucesso!' });
+
+            try {
+                await transport.sendMail({
+                    from: 'Suporte Cinerate <noreply@cinerate.com>',
+                    to: email,
+                    subject: 'Seu código de recuperação de senha',
+                    text: `Seu código de recuperação é: ${codigo}`,
+                    html: `<b>Seu código de recuperação é: ${codigo}</b><p>Ele expira em 15 minutos.</p>`
+                });
+
+                return res.json({ message: 'Email enviado com sucesso!' });
+            } catch (err) {
+                return res.status(500).json({ error: 'Erro ao enviar email' });
+            }
         } catch (err) {
-            return res.status(500).json({ error: 'Erro ao enviar email' });
+            console.log(err)
+            return res.status(500).json({ error: 'Erro interno do servidor!'});
         }
     }
 
 
     async resetaSenha(req, res) {
-        const { email, codigo, password } = req.body;
+        try {
+            const { email, codigo, password, confirmPassword } = req.body;
 
-        const usuario = await Usuarios.findOne({ where: { email } });
+            const usuario = await Usuarios.findOne({ where: { email } });
 
-        if (!usuario || usuario.password_reset_codigo !== codigo) {
-            return res.status(400).json({ error: 'Código inválido' });
+            if (!usuario || usuario.password_reset_codigo !== codigo) {
+                return res.status(400).json({ error: 'Código inválido' });
+            }
+
+            if (new Date() > usuario.password_reset_expira) {
+                return res.status(400).json({ error: 'Código expirado' });
+            }
+
+            if (password != confirmPassword) {
+                return res.status(401).json({error: 'As senhas não coincidem!'})
+            }
+
+            usuario.password = password; 
+            usuario.password_reset_codigo = null;
+            usuario.password_reset_expira = null;
+
+            await usuario.save();
+
+            return res.json({ message: 'Senha alterada com sucesso!' });
+
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({ error: 'Erro interno do servidor!'});
         }
-
-        if (new Date() > usuario.password_reset_expira) {
-            return res.status(400).json({ error: 'Código expirado' });
-        }
-
-        usuario.password = password; 
-        usuario.password_reset_codigo = null;
-        usuario.password_reset_expira = null;
-
-        await usuario.save();
-
-        return res.json({ message: 'Senha alterada com sucesso!' });
     }
 
 
