a4db17c520b774fb8b55eb7525c5ddc92e1c8642
João Victor Arantes Oliveira <ojoao830@gmail.com>
2026-05-19 09:15:49 -0300

    feat: cria função update filme

--- back/src/apps/controllers/FilmeController.js
+++ back/src/apps/controllers/FilmeController.js
@@ -34,6 +34,65 @@ class FilmeController {
             return res.status(500).json({ error: "Erro interno no servidor!" });
         }
     }
+
+    async delete(req, res){
+        try {
+            const { id } = req.params;
+
+            const filme = await Filmes.findByPk(id);
+            
+            if (!filme) return res.status(400).json({ error: "Filme não existe" });
+
+            const filmeDeletado = await Filmes.destroy({
+                where: {
+                    id,
+                }
+            });
+            
+            if (!filmeDeletado) {
+                return res.status(400).json({error: 'Falha ao deletar filme!'})
+            }
+
+            return res.status(200).json({ message: "Filme deletado!" });
+
+        } catch (error) {
+            return res.status(500).json({ error: "Erro interno no servidor!" });
+        }
+    }
+
+    async update(req, res){
+        try {
+            const { id } = req.params;
+
+            const filme = await Filmes.findByPk(id);
+            
+            if (!filme) return res.status(400).json({ error: "Filme não existe" });
+
+            const { capa, titulo, genero, sinopse, ano } = req.body;
+
+            if (capa) filme.capa = capa;
+            if (titulo) filme.titulo = titulo;
+            if (genero) filme.genero = genero;
+            if (sinopse) filme.sinopse = sinopse;
+            if (ano) {
+                if (ano > new Date().getFullYear() || ano <= 0) {
+                    return res.status(400).json({ error: "Coloque uma data válida!" });
+                }
+                filme.ano = ano;
+            }
+
+            const filmeAtualizado = await filme.save();
+
+            if (!filmeAtualizado) {
+                return res.status(400).json({error: 'Falha ao deletar filme!'})
+            }
+
+            return res.status(200).json({ message: "Filme atualizado!" });
+
+        } catch (error) {
+            return res.status(500).json({ error: "Erro interno no servidor!" });
+        }
+    }
 }
 
 module.exports = new FilmeController();
--- back/src/routes.js
+++ back/src/routes.js
@@ -8,11 +8,12 @@ const UsuarioController = require('./apps/controllers/UsuarioController');
 const FilmeController = require('./apps/controllers/FilmeController');
 
 const authSchema = require('./schemas/auth_schema.json');
-const createUsuarioSchema = require('./schemas/create_usuario_schema.json');
-const createFilmeSchema = require('./schemas/create_filme_schema.json');
 const codigoSchema = require('./schemas/codigo_schema.json');
 const resetPasswordSchema = require('./schemas/reset_password_schema.json');
+const createUsuarioSchema = require('./schemas/create_usuario_schema.json');
+const createFilmeSchema = require('./schemas/create_filme_schema.json');
 const updateUsuarioSchema = require('./schemas/update_usuario_schema.json');
+const updateFilmeSchema = require('./schemas/update_filme_schema.json');
 
 const routes = new Router();
 
@@ -21,17 +22,20 @@ routes.post('/login', schemaValidator(authSchema), AutenticadorController.autent
 routes.post('/login/recuperar_senha', schemaValidator(codigoSchema), AutenticadorController.esqueciSenha);
 routes.put('/login/recuperar_senha/confirmar', schemaValidator(resetPasswordSchema), AutenticadorController.resetaSenha);
 routes.post('/filme', schemaValidator(createFilmeSchema), FilmeController.create);
+routes.delete('/delete-filme/:id', FilmeController.delete);
+routes.put('/update-filme/:id', schemaValidator(updateFilmeSchema), FilmeController.update);
+
 routes.use(AutenticadorMiddleware);
 
 routes.put('/user/update', schemaValidator(updateUsuarioSchema), UsuarioController.update);
 routes.delete('/user/delete', UsuarioController.delete);
-
-
 
 routes.get('/', (req, res) => {
     return res.send({message: 'Connected'});
 });
 
 routes.use(AutenticadorIsAdmin);
+
+
 
 module.exports = routes;
