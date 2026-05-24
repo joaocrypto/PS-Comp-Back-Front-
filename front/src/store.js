4ec5f5b095237b9c590457fdabe36e903f8ded75
João Victor Arantes Oliveira <ojoao830@gmail.com>
2026-05-24 11:26:14 -0300

    feat: adiciona os reducers de filme e avaliacao

--- front/src/store.js
+++ front/src/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from "./slices/authSlice";
+import filmeReducer from "./slices/filmeSlice";
+import avaliacaoReducer from "./slices/avaliacaoSlice"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        filme: filmeReducer,
+        avaliacao: avaliacaoReducer,
     },
 });
