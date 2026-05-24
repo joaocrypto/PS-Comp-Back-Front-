2bda22ab7c5160ad70bc0844c2bbd21bab6f0f67
João Victor Arantes Oliveira <ojoao830@gmail.com>
2026-05-24 18:21:02 -0300

    feat: cria pagina home e suas funcionalidades

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,9 +1,50 @@
 import "./Home.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listAll } from "../../slices/filmeSlice";
+import { useNavigate } from "react-router-dom";
+import Message from "../../components/Message";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default Home
+  const { filmes, loading, error } = useSelector((state) => state.filme);
+
+  useEffect(() => {
+    dispatch(listAll());
+  }, [dispatch]);
+
+  return (
+    <div id="home-page">
+      <h1 className="home-titulo">Catálogo de Filmes</h1>
+
+      {loading ? (
+        <Message msg="Carregando catálogo de filmes..." type="msg" />
+      ) : error ? (
+        <Message msg={error} type="error" />
+      ) : (
+        <div className="filmes-list">
+          {filmes.length > 0 ? (
+            filmes.map((filme) => (
+              <div className="filme-item-card" key={filme.id} onClick={() => navigate(`/filme/${filme.id}`)}>
+                <div className="filme-item-capa">
+                  {filme.capa ? (
+                    <img src={filme.capa} alt={filme.titulo} />
+                  ) : (
+                    <span>Sem imagem</span>
+                  )}
+                </div>
+                <h3 className="filme-item-titulo">{filme.titulo}</h3>
+              </div>
+            ))
+          ) : (
+            <p className="sem-filmes">Nenhum filme cadastrado no momento.</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
